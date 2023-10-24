246f378407748c062f774b91f02c6afcbdcd800e
JeongUijeong <jeong275117@gmail.com>
2023-10-24 19:09:45 +0900

    feat: 여행 Entity에 메서드 추가
    - 여행 조회 응답 DTO로 변환하는 메서드 추가
    - 여행 정보 수정 메서드 추가
    - 날짜 변환 메서드 추가(global util 클래스가 만들어지면 수정할 것)

--- src/main/java/com/fc/toy_project2/domain/trip/entity/Trip.java
+++ src/main/java/com/fc/toy_project2/domain/trip/entity/Trip.java
@@ -1,6 +1,8 @@
 package com.fc.toy_project2.domain.trip.entity;
 
 import com.fc.toy_project2.domain.itinerary.entity.Itinerary;
+import com.fc.toy_project2.domain.trip.dto.request.UpdateTripRequestDTO;
+import com.fc.toy_project2.domain.trip.dto.response.TripResponseDTO;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,13 +10,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 
+/**
+ * Trip Entity
+ */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
@@ -35,4 +42,67 @@ public class Trip {
 
     @OneToMany(mappedBy = "trip", cascade = CascadeType.ALL)
     private List<Itinerary> itineraries = new ArrayList<>();
+
+    /**
+     * Builder 패턴이 적용된 Trip Entity 생성자
+     *
+     * @param id          여행 ID
+     * @param name        여행 이름
+     * @param startDate   여행 시작일
+     * @param endDate     여행 종료일
+     * @param itineraries 해당 여행의 여정 리스트
+     */
+    @Builder
+    public Trip(Long id, String name, LocalDate startDate, LocalDate endDate, Boolean isDomestic,
+        List<Itinerary> itineraries) {
+        this.id = id;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isDomestic = isDomestic;
+        this.itineraries = itineraries;
+    }
+
+    /**
+     * Trip Entity를 TripResponseDTO로 변환
+     *
+     * @return TripResponseDTO
+     */
+    public TripResponseDTO toTripResponseDTO() {
+        return TripResponseDTO.builder().id(this.id).name(this.name)
+            .startDate(localDateToString(this.startDate)).endDate(localDateToString(this.endDate))
+            .isDomestic(this.isDomestic).build();
+    }
+
+    /**
+     * 여행 정보를 수정
+     *
+     * @param updateTripRequestDTO 여행 정보 수정 요청 DTO
+     */
+    public void updateTrip(UpdateTripRequestDTO updateTripRequestDTO) {
+        this.name = updateTripRequestDTO.getName();
+        this.startDate = stringToLocalDate(updateTripRequestDTO.getStartDate());
+        this.endDate = stringToLocalDate(updateTripRequestDTO.getEndDate());
+        this.isDomestic = updateTripRequestDTO.getIsDomestic();
+    }
+
+    /**
+     * LocalDate 타입을 String 타입으로 변환
+     *
+     * @param date LocalDate 타입의 날짜
+     * @return String 타입의 날짜
+     */
+    private String localDateToString(LocalDate date) {
+        return date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    /**
+     * String 타입을 LocalDate 타입으로 변환
+     *
+     * @param date String 타입의 날짜
+     * @return LocalDate 타입의 날짜
+     */
+    private LocalDate stringToLocalDate(String date) {
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
 }
