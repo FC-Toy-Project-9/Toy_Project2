8776758dfd50d8884e0ea6039a0c5c2273c3dd82
Yurim Yang <yang980329@likelion.org>
2023-10-26 08:08:25 +0900

    fix: Dependency Injection문제 해결

--- src/main/java/com/fc/toy_project2/domain/itinerary/controller/ItineraryGetDeleteController.java
+++ src/main/java/com/fc/toy_project2/domain/itinerary/controller/ItineraryGetDeleteController.java
@@ -1,8 +1,11 @@
 package com.fc.toy_project2.domain.itinerary.controller;
 
 
+import com.fc.toy_project2.domain.itinerary.dto.response.ItineraryGetResponseDTO;
+import com.fc.toy_project2.domain.itinerary.dto.response.ItinerarySearchResponseDTO;
 import com.fc.toy_project2.domain.itinerary.service.ItineraryGetDeleteService;
 import com.fc.toy_project2.global.DTO.ResponseDTO;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
@@ -21,7 +24,7 @@ public class ItineraryGetDeleteController {
     private final ItineraryGetDeleteService itineraryGetDeleteService;
 
     @GetMapping("/keyword")
-    public ResponseEntity<ResponseDTO> getItinerarySearch(@RequestParam String query)
+    public ResponseEntity<ResponseDTO<List<ItinerarySearchResponseDTO>>> getPlaceByKeyword(@RequestParam String query)
         throws Exception {
         return ResponseEntity.status(HttpStatus.OK).body(
             ResponseDTO.res(HttpStatus.OK, itineraryGetDeleteService.getPlaceByKeyword(query),
@@ -30,14 +33,14 @@ public class ItineraryGetDeleteController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<ResponseDTO> getItineraryByTripId(@RequestParam long tripId) {
+    public ResponseEntity<ResponseDTO<List>> getItineraryByTripId(@RequestParam long tripId) {
         return ResponseEntity.status(HttpStatus.OK).body(
             ResponseDTO.res(HttpStatus.OK, itineraryGetDeleteService.getItineraryByTripId(tripId),
                 "성공적으로 여정을 조회했습니다."));
     }
 
     @DeleteMapping("")
-    public ResponseEntity<ResponseDTO> deleteItinerary(@RequestParam long itineraryId)
+    public ResponseEntity<ResponseDTO<ItineraryGetResponseDTO>> deleteItinerary(@RequestParam long itineraryId)
         throws NotFoundException {
         return ResponseEntity.status(HttpStatus.OK).body(
             ResponseDTO.res(HttpStatus.OK, itineraryGetDeleteService.deleteItinerary(itineraryId),
