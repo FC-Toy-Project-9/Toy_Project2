b34cb4eeb8b1f93d4ea495125a76210e426c4f35
unknown <dldmldlsy@naver.com>
2023-10-25 16:24:14 +0900

    refactor: 일시 포맷 기능 테스트코드 불필요한 코드 삭제

--- src/test/java/com/fc/toy_project2/global/util/DateTypeFormatterUtilTest.java
+++ src/test/java/com/fc/toy_project2/global/util/DateTypeFormatterUtilTest.java
@@ -8,10 +8,8 @@ import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
+
 class DateTypeFormatterUtilTest {
 
     @Nested
