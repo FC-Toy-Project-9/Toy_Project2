4c37fa0a01ddd69c7d2d4be238777845669c815a
JeongUijeong <jeong275117@gmail.com>
2023-10-27 17:55:40 +0900

    refactor: 단순 유지 규칙에 따라 요청 URI에 복수형 사용

--- src/main/java/com/fc/toy_project2/domain/trip/controller/TripRestController.java
+++ src/main/java/com/fc/toy_project2/domain/trip/controller/TripRestController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/trip")
+@RequestMapping("/api/trips")
 public class TripRestController {
 
     private final TripService tripService;
